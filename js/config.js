8cf079ba98807dbc83e4d695b775752be8a318d0
Aleksandr Katelnikov <aleksandr.katelnikov@x5.ru>
2022-07-27 12:17:36 +0300

    Commit for comments

--- js/config.js
+++
@@ -1,7 +0,0 @@
-export const BALLS_AMOUNT = 20;
-export const BALL_DIAMETER = 40;
-export const FRICTION_FACTOR_LEFT = 0.95;
-export const FRICTION_FACTOR_RIGHT = 0.999;
-export const VELOCITY_THRESHOLD = 0.1;
-export const STORAGE = 'idb';
-export const AUTOSAVE_INTERVAL = 1000;
